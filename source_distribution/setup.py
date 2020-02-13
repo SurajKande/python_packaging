df9481fa21157f9dcdee5d299f444f7b7f58aae8
SurajKande <37841586+SurajKande@users.noreply.github.com>
2020-02-13 10:00:47 +0530

    Update setup.py

--- source_distribution/setup.py
+++ source_distribution/setup.py
@@ -1,1 +1,11 @@
-from setuptools import 
+from setuptools import setup  
+
+setup( 
+      name = 'python_source_distribution_packaging', 
+      version = '1.0.0', 
+      py_modules = ['addition'], 
+      author ='Suraj Kande', 
+      #author_email = 'surajkande1298@gmail.com', 
+      description = 'a simple program to add two numbers', 
+      keywords='adds two numbers', 
+) 
