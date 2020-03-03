efec5a5291f510f114fcef4e2a2eff4b752ae078
SurajKande <37841586+SurajKande@users.noreply.github.com>
2020-03-03 13:13:32 +0530

    Update setup.py

--- sample_ML_classification_package/setup.py
+++ sample_ML_classification_package/setup.py
@@ -1,3 +1,61 @@
+##########################################################################
+## Imports
+#########################################################################
 from setuptools import setup
-from setuptools import find_packages()
+from setuptools import find_packages
 
+##########################################################################
+## Package Information
+##########################################################################
+
+## Basic information
+NAME         = "ML_models"
+DESCRIPTION  = "creating a package of ML classification model, will printout accuracy"
+AUTHOR       = "abcd"
+EMAIL        = "abcd@abcd.com"
+PACKAGE      = "classification"
+URL          = "https://github.com/SurajKande/python_packaging/tree/master/sample_ML_classification_package"
+KEYWORDS     = (
+    'ML', 
+    'python', 
+    'Automate',
+    'classification',
+)
+
+## Important Paths
+#REQUIRE_PATH = "requirements.txt"
+
+## Directories to ignore in find_packages
+EXCLUDES     = (
+    "tests", "resources", "notebooks",
+)
+
+##########################################################################
+## Helper Functions
+##########################################################################
+
+
+##########################################################################
+## Define the configuration
+##########################################################################
+
+config = {
+    "name": NAME,
+    "description": DESCRIPTION,
+    "author": AUTHOR,
+    "author_email": EMAIL,
+    "url": URL,
+    "maintainer": AUTHOR,
+    "maintainer_email": EMAIL,
+    "packages": find_packages(exclude=EXCLUDES),
+    "install_requires": ['pandas', 'numpy', 'matplotlib', 'seaborn', 'scipy','sklearn'],
+    "keywords": KEYWORDS,
+    "zip_safe": False,
+}
+
+##########################################################################
+## Run setup script
+##########################################################################
+
+if __name__ == '__main__':
+    setup(**config)
