e9bdf189878a3d62834439c912922cdbb080a3d9
SurajKande <37841586+SurajKande@users.noreply.github.com>
2020-04-03 18:52:57 +0530

    Update decision_tree_classifier.py

--- sample_ML_classification_package/classification/decision_tree_classifier.py
+++ sample_ML_classification_package/classification/decision_tree_classifier.py
@@ -1,12 +1,11 @@
 def decision_tree_classifier( X_train, X_test, Y_train, Y_test ):
-	{
-			decision_tree = DecisionTreeClassifier(criterion="entropy")
-			decision_tree.fit(X_train,Y_train)
-			Y_pred = decision_tree.predict(X_test)
+	
+      decision_tree = DecisionTreeClassifier(criterion="entropy")
+      decision_tree.fit(X_train,Y_train)
+      Y_pred = decision_tree.predict(X_test)
       
       print("accuracy score for training data")
 			print(round(decision_tree.score(X_train,Y_train)*100,2))
       
       print("accuracy score for testing data")
 			print(round(decision_tree.score(X_test,Y_test)*100,2))
-	}
